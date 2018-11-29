39111157091dcd98ce65db3c6c9db56467224dec
jiangzhuo <glmmmmmm@163.com>
2018-11-29 20:38:44 +0800

    数据库放到一个库里

--- src/user.module.ts
+++ src/user.module.ts
@@ -33,7 +33,8 @@ import { UserController } from './controllers/user.controller';
             log: 'trace',
         }),
         MongooseModule.forRoot('mongodb://sati:kjhguiyIUYkjh32kh@dds-2zee21d7f4fff2f41890-pub.mongodb.rds.aliyuncs.com:3717,dds-2zee21d7f4fff2f42351-pub.mongodb.rds.aliyuncs.com:3717/sati?replicaSet=mgset-9200157',
-            // MongooseModule.forRoot('mongodb://localhost:27017/module_user',
+        //     MongooseModule.forRoot('mongodb://localhost:27017/sati',
+        //     MongooseModule.forRoot('mongodb://root:kjhguiyIUYkjh32kh@dds-2ze5f8fcc72702b41188-pub.mongodb.rds.aliyuncs.com:3717,dds-2ze5f8fcc72702b42191-pub.mongodb.rds.aliyuncs.com:3717/sati?replicaSet=mgset-10924097&authDB=admin',
             { connectionName: 'sati', useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true }),
         MongooseModule.forFeature([{ name: 'User', schema: UserSchema, collection: 'user' }], 'sati'),
         MongooseModule.forFeature([{ name: 'Account', schema: AccountSchema, collection: 'account' }], 'sati')
